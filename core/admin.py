19cbb4ff3f8069a26c06531ec64f7380d7708039
Sabin Subedi <sabinsubedi125@gmail.com>
2022-11-18 15:49:23 +0545

    implemented basic auth system

--- core/admin.py
+++ core/admin.py
@@ -3,7 +3,19 @@ from django.contrib import admin
 from django.contrib.contenttypes.admin import GenericTabularInline
 from store.admin import ProductAdmin
 from tags.models import TaggedItem
-
+from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
+from .models import User
+@admin.register(User)
+class UserAdmin(BaseUserAdmin):
+    add_fieldsets = (
+        (
+            None,
+            {
+                "classes": ("wide",),
+                "fields": ("username", "email","first_name","last_name","password1", "password2"),
+            },
+        ),
+    )
 
 class TagInline(GenericTabularInline):
     autocomplete_fields = ['tag']
