b3d2879cafb38ef57bd08a4279417416fa57cc0d
Sabin Subedi <sabinsubedi125@gmail.com>
2022-12-25 14:14:02 +0545

    minor update

--- storefront/settings.py
+++ storefront/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+from datetime import timedelta
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -154,6 +155,8 @@ REST_FRAMEWORK = {
 
 SIMPLE_JWT = {
     'AUTH_HEADER_TYPES': ('JWT',),
+    'ACCESS_TOKEN_LIFETIME': timedelta(minutes=5),
+    'REFRESH_TOKEN_LIFETIME': timedelta(seconds=10),
 }
 
 DJOSER = {
